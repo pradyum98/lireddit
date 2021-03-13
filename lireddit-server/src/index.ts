890bc3179e0b6253440436db2db0558780be28bf
Pradyum Menon <pj7446@gmail.com>
2021-03-13 18:25:52 +0530

    First commit

---
+++ lireddit-server/src/index.ts
@@ -0,0 +1,36 @@
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/posts";
+import { UserResolver } from "./resolvers/user";
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
+
+  const app = express();
+  app.get("/", (_, res) => {
+    res.send("hello");
+  });
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
+  
+  const apolloServer = new ApolloServer({
+      schema: await buildSchema({
+          resolvers:[HelloResolver,PostResolver,UserResolver],
+          validate: false
+      }),
+      context: () => ({ em: orm.em})
+  })
+
+  apolloServer.applyMiddleware({app});
+};
+
+main().catch((err) => {
+  console.log(err);
+});
